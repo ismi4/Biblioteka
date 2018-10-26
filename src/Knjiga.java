2608390e316cab698daba89ab6619fff47668ba9
Ismail Brkic <brkicismail@gmail.com>
2018-10-26 12:18:57 -0700

    Biblioteka - napisane klase Racun i Knjiga, test nije zavrsen

---
+++ src/Knjiga.java
@@ -0,0 +1,16 @@
+
+public class Knjiga {
+
+    public static int brojKnjige;
+    public static String imeKnjige;
+    public static boolean status;
+    
+    Knjiga (int brojKnjige, String imeKnjige, boolean status){
+	this.brojKnjige = brojKnjige;
+	this.imeKnjige = imeKnjige;
+	this.status = status;
+    }
+    
+
+    
+}
---
+++ src/Racun.java
@@ -0,0 +1,15 @@
+
+public class Racun {
+
+    public static int brojRacuna;
+    public static String ime;
+    public static int brojPosudenihKnjiga;
+    
+    //kreiranje racuna
+    Racun(int brojRacuna, String ime, int brojPosudenihKnjiga){
+	this.brojRacuna = brojRacuna;
+	this.ime = ime;
+	this.brojPosudenihKnjiga = brojPosudenihKnjiga;
+    }
+    
+}
